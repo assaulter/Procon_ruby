3e03cafc09bc84a65a47c407e30f72e51e35bf3e
Kazuki Kubo <kz@assaulter.org>
2013-10-13 23:59:54 +0900

    範囲の方の素数問題まで作成。
    Signed-off-by: Kazuki Kubo <kz@assaulter.org>

--- eratos/eratos.rb
+++ eratos/eratos.rb
@@ -2,24 +2,43 @@ class Eratos
   # この書き方だとclass << self ... end内は、クラスメソッドになる
   class << self
     def eratos(num: num)
-      num==1 ? 0 : create_prime_list(list:(2..num).to_a).count
-      # 2..numを作る(偶数をドロップしておく)
+      num==1 ? 0 : sieve(list:(2..num).to_a, prime_list: []).count
+      # 2..numを作る
       # 篩にかける
       # 個数を返す
     end
 
-    # private
+    def eratos_by_range(from: from, to: to)
+      to==2 ? 1 : sieve_by_range(from: from, to: to).count
+    end
 
-    # 整数のリストから、素数のみのリストを作成する
-    # input: 2以上の整数のリスト, prime_listが入る入れ物
-    def create_prime_list(list: list) 
-      unless list.empty?
-        head = list.first
+    # 範囲内の素数リストを返す
+    def sieve_by_range(from: from, to: to)
+        # (a,b] に対して、予め求めておく素数は, sqrt(b)まで→試し割り、エラトスと一緒
+        sqrt_to = Math.sqrt(to).to_i
+        prime_list = sieve(list: (2..sqrt_to).to_a, prime_list: [])
+        # この素数リストを用いて、from..toなリストを篩にかける
+        list = (from..to).to_a
+        prime_list.each do |prime|
+          list.reject!{|item| item%prime == 0}
+        end
+
+        list
+    end
+
+    # ちゃんとしたエラトステネスの篩を実装
+    # list:入力, prime_list:素数のリスト
+    def sieve(list: list, prime_list: prime_list)
+      # 停止条件:探索リストの先頭=headが、head>sqrt(num)になるまで
+      head = list.first
+      unless head*head>list.last
+        prime_list << head
         list.reject!{|item| item%head == 0}
-        create_prime_list(list: list).unshift(head)
+        sieve(list: list, prime_list: prime_list)
       end
 
-      return list
+      return prime_list + list
     end
+
   end
 end
--- eratos/eratos_spec.rb
+++ eratos/eratos_spec.rb
@@ -19,19 +19,39 @@ describe Eratos do
     end
   end
 
-  context '13を与えられた時' do
-    it '6を返す' do
-      # 2 3 5 7 11 13があるので6
-    end
-  end
-
-  context 'create_prime_listに13までの整数のリストを与える場合' do
-    it '[2,3,5,7,11,13]を返す' do
-      expected = [2,3,5,7,11,13]
-      actual = Eratos.create_prime_list(list: (2..13).to_a)
+  context '10_000が与えられた時' do
+    it '1229を返す' do
+      expected = 1229
+      actual = Eratos.eratos(num: 10000)
 
       expect(actual).to eq expected
     end
   end
 
+  context 'sieveに120までの整数リストを与えた場合' do
+    it '[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]を返す' do
+      expected = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+      actual = Eratos.sieve(list: (2..120).to_a, prime_list:[])
+
+      expect(actual).to eq expected
+    end
+  end
+
+  context '範囲内の素数のリストを求める' do
+    it '50~120の場合は、[53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]を返す' do
+      expected = [53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+      actual = Eratos.sieve_by_range(from: 50, to:120)
+
+      expect(actual).to eq expected
+    end
+  end
+
+  context '範囲内の素数の数を求める' do
+    it '50~120の場合は15個' do
+      expected = 15
+      actual = Eratos.eratos_by_range(from: 50, to: 120)
+
+      expect(actual).to eq expected
+    end
+  end
 end
