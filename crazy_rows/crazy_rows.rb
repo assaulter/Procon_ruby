0d8b3977c246f6ac855f56d903685430ad7a326a
久保 和貴 <k-kubo@biglobe.co.jp>
2013-10-29 16:54:08 +0900

    crazyRowsを変更。
    Signed-off-by: 久保 和貴 <k-kubo@biglobe.co.jp>

--- crazy_rows/crazy_rows.rb
+++ crazy_rows/crazy_rows.rb
@@ -4,6 +4,26 @@ class CrazyRows
     def cr(list: list)
       # 配列の最後に出現する１のインデックスのリストを作成
       lastone_list = index_list(list: list)
+      # バブルソートを行い、入れ替え回数をカウントする
+      count = 0
+
+      list.each_with_index {|elem, i|
+        (0..(list.length - i)).each_with_index {|num, index|
+          list[index], list[index+1] = list[index+1], list[index] if list[index] > list[index+1]
+        }
+      }
+
+      list
+    end
+
+    def bubble(list: list)
+      list.each_with_index {|elem, i|
+        (0..(list.length - i)).each_with_index {|num, index|
+          list[index], list[index+1] = list[index+1], list[index] if list[index] > list[index+1]
+        }
+      }
+
+      list      
     end
 
     # private
